96b495addad642830830dfebc6fa7de5d718f33c
citefred <citefred@gmail.com>
2026-05-06 16:31:39 +0900

    feat: PlayerPawn 컴포넌트 구성 - BoxComponent/StaticMeshComponent 생성자에 추가

--- Source/ShootingCPP/Private/PlayerPawn.cpp
+++ Source/ShootingCPP/Private/PlayerPawn.cpp
@@ -3,12 +3,30 @@
 
 #include "PlayerPawn.h"
 
+#include "Components/BoxComponent.h"
+
 
 // Sets default values
 APlayerPawn::APlayerPawn()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	// 박스 콜리전 컴포넌트 생성
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+	
+	// 생성한 박스 콜리전 컴포넌트를 최상단 컴포넌트로 설정
+	SetRootComponent(boxComp);
+	
+	// 스태틱메시 컴포넌트 생성
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My Mesh Component"));
+	
+	// 박스 콜리전 자식으로 스태틱 메시 컴포넌트 설정
+	meshComp->SetupAttachment(boxComp);
+	
+	// 박스 콜라이더 크기를 50x50x50 설정
+	FVector boxSize = FVector(50.f, 50.f, 50.f);
+	boxComp->SetBoxExtent(boxSize);
 }
 
 // Called when the game starts or when spawned
