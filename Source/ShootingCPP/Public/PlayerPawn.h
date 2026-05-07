ebfceff727ac289fe43c7f588603fc3f6a5d048f
citefred <citefred@gmail.com>
2026-05-07 12:30:05 +0900

    feat: PlayerPawn 총알 발사 로직 구현 - ArrowComponent, SpawnActor 연결

--- Source/ShootingCPP/Public/PlayerPawn.h
+++ Source/ShootingCPP/Public/PlayerPawn.h
@@ -45,16 +45,31 @@ public:
 	UPROPERTY(EditAnywhere)
 	class UInputAction* iaVertical;
 	
+	UPROPERTY(EditAnywhere)
+	class UInputAction* iaFire;
+	
 	// 이동 속도 변수
 	UPROPERTY(EditAnywhere)
 	float moveSpeed = 500.f;
 	
+	// 총알 발사 위치(총구)
+	// UArrowComponent는 직선상 경로를 활용하는 클래스
+	UPROPERTY(EditAnywhere)
+	class UArrowComponent* firePosition;
+
+	// 생성할 총알 블루프린트
+	// 월드에 배치하지 않은 원본파일을 변수에 할당하기 위해서 TSubclassOf<T> 타입 사용
+	// <T>에 생성될 블루프린트(여기선, BP_Bullet)가 상속한 액터 클래스를 넣어준다.
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class ABullet> bulletFactory;
+	
 private:
-	// 상요자 키 입력값을 받을 변수
+	// 사용자 키 입력값을 받을 변수
 	float h;
 	float v;
 	
 	// 입력 이벤트 발동 시 실행 될 함수
 	void OnInputHorizontal(const struct FInputActionValue& value);
 	void OnInputVertical(const struct FInputActionValue& value);
+	void Fire();
 };
