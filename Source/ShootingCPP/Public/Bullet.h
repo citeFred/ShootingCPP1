d71f848ef74dac90fdb60f8f2166d8c5d4f5cb31
citefred <citefred@gmail.com>
2026-05-08 12:22:05 +0900

    feat: 폭발 파티클 이펙트 구현(구버전 Kismet)

--- Source/ShootingCPP/Public/Bullet.h
+++ Source/ShootingCPP/Public/Bullet.h
@@ -35,6 +35,10 @@ public:
 	UPROPERTY(EditAnywhere)
 	float moveSpeed = 800.f;
 	
+	// 폭발 파티클 이펙트
+	UPROPERTY(EditAnywhere)
+	class UParticleSystem* explosionFX;
+	
 	// 충돌 이벤트 함수 - OnComponentBeginOverlap 델리게이트에 등록할 함수
 	UFUNCTION()
 	void OnBulletOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
